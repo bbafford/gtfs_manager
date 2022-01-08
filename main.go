76568ff66f27ab16cd2ccc4374ed8dbbd96d0674
bbafford <brock.bafford@gmail.com>
2022-01-07 19:54:53 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+
+	fmt.Println("Loading schedule")
+}
+
+func LoadSchedule(strPath string){
+}
+
+func ListStopsbyRoute(strRouteId string){
+
+}
+
